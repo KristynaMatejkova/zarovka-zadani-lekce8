18408a433b7a0ce46a2f2d5f3e1e442e0fc5cd70
LAPTOP-6A2IC92J\rdhd1 <junova.kristyna@gmail.com>
2023-03-12 17:59:28 +0100

    cviceni3

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('funguju!');
+
+/*
+ let zarovka = document.querySelector('.bulb')
+
+ const rozsvitit = () => {
+    zarovka.classList.toggle('bulb--on')
+ }
+
+document.addEventListener('keydown', rozsvitit)
+*/
+
+const zarovka = document.querySelector('.bulb')
+
+document.addEventListener('keydown', () => {
+	zarovka.classList.toggle('bulb--on')
+})
